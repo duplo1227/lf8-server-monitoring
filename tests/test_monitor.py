d617b5ec1a21c1aa3cf7148e7e0517ee81e4015c
duplo1227 <zarinaanichkina@gmail.com>
2025-10-08 12:27:58 +0200

    pipeline added

---
+++ tests/test_monitor.py
@@ -0,0 +1,8 @@
+import psutil
+from src.monitor import get_system_metrics
+
+def test_metrics_return_values():
+    cpu, mem, disk = get_system_metrics()
+    assert 0 <= cpu <= 100
+    assert 0 <= mem <= 100
+    assert 0 <= disk <= 100
