5a3540251d099d38bae3f0f62c5b707e092b1c8b
duplo1227 <zarinaanichkina@gmail.com>
2025-10-09 14:16:16 +0200

    pipeline fixed one more time...

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -6,7 +6,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.monitor import get_system_metrics
 
 def test_metrics_return_values():
-    cpu, mem, disk = get_system_metrics()
+    cpu, mem, disk, proc = get_system_metrics()
     assert 0 <= cpu <= 100
     assert 0 <= mem <= 100
     assert 0 <= disk <= 100
+    assert 0 <= proc <= 200
