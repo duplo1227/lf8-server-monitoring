ae54b0a8e889525746a94312d9d793998e5638bd
duplo1227 <zarinaanichkina@gmail.com>
2025-10-14 12:22:05 +0200

    importing changed

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -3,7 +3,7 @@ import os
 import psutil
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.alarm import .
+from src.alarm import *
 from src.monitor import get_metrics
 
 def test_metrics_return_values():
