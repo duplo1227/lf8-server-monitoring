930fdc51601c6ba2930117ba844a243e81b24aaa
TaubesNuesschen <pbank@netfonds.de>
2025-10-14 10:57:18 +0200

    monitor + alarm

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -3,11 +3,11 @@ import os
 import psutil
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.monitor import get_system_metrics
+from src.monitor import get_metrics
 
 def test_metrics_return_values():
-    cpu, mem, disk, proc = get_system_metrics()
-    assert 0 <= cpu <= 100
-    assert 0 <= mem <= 100
-    assert 0 <= disk <= 100
-    assert 0 <= proc <= 190
+    a = get_metrics()
+    assert 0 <= a['cpu'] <= 100
+    assert 0 <= a['mem'] <= 100
+    assert 0 <= a['disk'] <= 100
+    assert 0 <= a['proc'] <= 190
